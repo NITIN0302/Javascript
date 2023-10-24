4a221654b3b065c0ccfffe9896d754bbd1e04d98
Nitin srivastva <nitinsrivastava2all@gmail.com>
2023-10-25 00:33:00 +0530

    ForEach Loop

---
+++ Loops/modified_loops.js
@@ -0,0 +1,24 @@
+const obj1 = {
+    name:"Nitin Srivastva",
+    price:"999",
+    time:"2 Hours",
+    system:"Window 10"
+}
+
+for (const key in obj1) 
+{
+    console.log(key, "->" , obj1[key])
+}
+
+
+const coding = [1,2,3,4,5,6,7,8,9]
+
+coding.forEach((ele)=>{
+    console.log(ele);
+})
+
+
+// you can pass a function in forEach loop
+coding.forEach(function(ele){
+    console.log(ele+10);
+})
