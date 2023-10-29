2b9f2ddc45fafe4f9fa8cf2141e06e755ea6072f
Nitin srivastva <nitinsrivastava2all@gmail.com>
2023-10-30 01:12:16 +0530

    Project 5 Color Generator

---
+++ Project/Project5/Project5.js
@@ -0,0 +1,21 @@
+const hex = '0123456789ABCDEF';
+let intervalId = null;
+let start = document.querySelector('.start');
+let stop = document.querySelector('.stop');
+
+start.addEventListener('click',(ele)=>{
+    let color = '#';
+    intervalId = setInterval(() => {
+        for(let i=0;i<6;i++)
+        {
+            color += hex[Math.floor(Math.random()*16)];
+        }
+        document.body.style.backgroundColor = color;
+        color = '#'
+    }, 1000);
+});
+
+
+stop.addEventListener('click',(ele)=>{
+    clearInterval(intervalId);
+});
