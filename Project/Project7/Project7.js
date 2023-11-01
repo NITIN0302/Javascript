b8618327fa6f0f31b632c8891c584b3d93d96310
Nitin srivastva <nitinsrivastava2all@gmail.com>
2023-11-01 14:17:30 +0530

    Cart System Complete project

--- Project/Project7/Project7.js
+++ Project/Project7/Project7.js
@@ -1,12 +1,38 @@
 let a = document.querySelectorAll('#Acc')
-
+let item = document.querySelector('.items')
+let cart = document.querySelector('.cart');
 let Cartbtn = document.querySelector('#AddToCart')
+let rmvbtn = document.querySelector('#Remove')
 
 Cartbtn.addEventListener('click',(ele)=>{
     a.forEach((e)=>{
-        console.log(e.checked);
+        let parentLi = e.parentElement;
+        if(e.checked == true)
+        {
+            e.setAttribute('id','Crt')
+            let parentUl = parentLi.parentElement;
+            parentUl.removeChild(parentLi)
+            e.checked = false;
+            cart.appendChild(parentLi);
+        }
     })
 });
+
+
+rmvbtn.addEventListener('click',(ele)=>{
+    let v = document.querySelectorAll('#Crt')
+    v.forEach((e)=>{
+        let cartLi = e.parentElement;
+        if(e.checked == true)
+        {
+            e.setAttribute('id','Acc');
+            let cartUl = cartLi.parentElement;
+            cartUl.removeChild(cartLi)
+            e.checked = false;
+            item.appendChild(cartLi);
+        }
+    })
+})
 
 
 
